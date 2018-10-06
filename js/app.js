ae7e0ebb069be8e755fd0f1c007cc59aa2ee43d5
Rabbi Alam <frabbi.alam@gmail.com>
2018-10-06 08:56:26 +0200

    updated js file

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-const deck=document.querySelector('deck');
+const deck=document.querySelector('.deck');
 deck.addEventListener('click', function(event){
   const clickTarget= event.target;
   if (clickTarget.classList.contains('card')){
